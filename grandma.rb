8d1b745f9e80b6f06e1f20e4b2c93f099151a16a
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-05 19:58:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,8 @@ def speak_to_grandma(phrase)
     "HUH?! SPEACK UP, SONNY!"
   end
 end
-    
+speak_to_grandma("I LOVE YOU GRANDMA")
+speak_to_grandma("AAAAA")
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
